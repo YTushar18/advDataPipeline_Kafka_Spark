41fc6d4edb83156d3473744bac98de823da7937d
Tushar <tusharofficial96@gmail.com>
2023-12-01 03:46:51 -0800

    Updated code

--- config.py
+++ config.py
@@ -13,3 +13,12 @@ kafka_config = {'bootstrap.servers': 'localhost:9092'}
 # Kafka topic
 kafka_topic = 'marketdata'
 
+
+# Azure Storage account credentials
+account_name = 'cpsc531advdbproject'
+account_key = 'bn5E08o0+vC8sHyWUKUE0rxRzoitH4nA9pNQE9XMlG9uHTNn1t13GqsglOvN2xwAzQbgmrfabPfn+AStt3x4xQ=='
+
+# Name of the container in Azure Blob Storage
+container_name = 'market-historical-data'
+
+
